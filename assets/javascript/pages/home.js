e32f01e0d8538d92d07cb46d34149c9b37c881e1
daslicht <daslicht@ansolas.de>
2016-08-01 11:44:07 +0200

    Carousel adjustments

--- assets/javascript/pages/home.js
+++ assets/javascript/pages/home.js
@@ -103,25 +103,24 @@ $(document).ready(function() {
 			autoplaySpeed: 2000,
 			centerMode: false,
 			centerPadding: '0',
+			infinite: true,
 			responsive: [
 			{
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 3,
-					infinite: true,
-					dots: true
 				}
 			},
 			{
-				breakpoint: 632,
+				breakpoint: 992,
 					settings: {
 						slidesToShow: 2,
 						slidesToScroll: 2
 					}
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 562,
 					settings: {
 						slidesToShow: 1,
 						slidesToScroll: 1
